cb15a8a25fef5c88ef3bd7bd0f85aa21144ed865
sanmijingbrahma <bsanmijing@gmail.com>
2025-01-28 18:25:03 +0530

    Add another middleware to a specific route to make that route accessable for a specific time only

--- server-with-middlewares/index.js
+++ server-with-middlewares/index.js
@@ -1,5 +1,5 @@
 const express = require('express') 
-const PORT = 8000;
+const PORT = 8080;
 
 
 
@@ -8,11 +8,31 @@ const app = express();
 // Middleware to log the use-agent of incoming request.
 app.use((req,res,next)=>{
     console.log(`User-Agent : ${req.headers["user-agent"]}`);
+    next();
+    
+})
+
+app.use("/between",(req,res,next)=>{
+
+    const time = new Date();
+    let hour = time.getHours();
+    if(hour >=9 && hour<=17){
+        next();
+    } else{
+        res.status(401).send("You are not allowed at the moment")
+    
+    }
+    
     
 })
 
 app.get("/",(req,res)=>{
     res.send("Welcome to the root")
+})
+
+
+app.get("/between",(req,res)=>{
+    res.send("Wow, You are at correct time to access this website")
 })
 
 
